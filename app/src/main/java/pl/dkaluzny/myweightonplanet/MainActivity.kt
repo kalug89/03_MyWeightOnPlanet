cc3b28408ebc85df33c27182b9406538290a7253
kalug89 <kaluzny_david@hotmail.com>
2018-07-21 10:54:52 +0200

    Your Weight on Mats is DOne

--- app/src/main/java/pl/dkaluzny/myweightonplanet/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/myweightonplanet/MainActivity.kt
@@ -1,12 +1,27 @@
 package pl.dkaluzny.myweightonplanet
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val marsGravity = 0.38
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var weight = enterWeightId.text
+        showWeightButtonId.setOnClickListener {
+            var result = calculateWeight(weight.toString().toDouble())
+
+            resultTextViewvId.text = "Yout weight " + result.toString() + " on Mars"
+        }
+    }
+
+    fun calculateWeight(userWeight: Double): Double {
+        return userWeight * marsGravity
     }
 }
+
